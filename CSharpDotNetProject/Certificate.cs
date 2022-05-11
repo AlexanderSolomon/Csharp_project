9054b2587797c4461245b2c3834b3fab58536dc9
Fiedler12 <o.lyngholm@gmail.com>
2022-05-11 18:46:51 +0200

    Search function

--- CSharpDotNetProject/Certificate.cs
+++ CSharpDotNetProject/Certificate.cs
@@ -28,11 +28,137 @@ namespace CSharpDotNetProject
         private string originalPaintThickness; 
         private string originalPaintThicknessMeasured;
         private string paintConclusion;
-        private Boolean engineNumberVerified; 
-
-
+        private Boolean engineNumberVerified;
+        private Boolean originalPaint;
+        private Boolean serialCorrect;
+        private string serialCondition;
+        private string serialDamaged; 
 
         public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
 
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Registration
+        {
+            get { return registration; }
+            set { registration = value; }
+        }
+        public string SerialNumber
+        {
+            get { return serialNumber; }
+            set { serialNumber = value; }
+        }
+        public String Workshop
+        {
+            get { return workshop; }
+            set { workshop = value; }
+        }
+        public string Customer
+        {
+            get { return customer; }
+            set { customer = value; }
+        }
+        public string Contact
+        {
+            get { return contact; }
+            set { contact = value; }
+        }
+        public string Registrationcertificate
+        {
+            get { return registrationcertificate; }
+            set { registrationcertificate = value; }
+        }
+        public string RegistrationcertificateOriginal
+        {
+            get { return registrationcertificateOriginal; }
+            set { registrationcertificateOriginal = value; }
+        }
+        public string LastRegistration
+        {
+            get { return lastRegistration; }
+            set { lastRegistration = value; }
+        }
+        public string ETypeApproveNr
+        {
+            get { return eTypeApproveNr; }
+            set { eTypeApproveNr = value; }
+        }
+        public Boolean GearBoxVerified
+        {
+            get { return gearBoxVerified; }
+        }
+        public Boolean ServiceHistoryVerified
+        {
+            get { return serviceHistoryVerified; }
+            set { serviceHistoryVerified = value; }
+        }
+        public Boolean KeyControlled
+        {
+            get { return keyControlled; }
+            set { keyControlled = value; }
+        }
+        public Boolean DocumentedByBrochure
+        {
+            get { return documentedByBrochure; }
+            set { documentedByBrochure = value;}
+        }
+        public Boolean DocumentedByLab
+        {
+            get { return documentedByLab; }
+            set { documentedByLab = value; }
+        }
+        public Boolean DocumentedByCocDoc
+        {
+            get { return documentedByCocDoc; }
+            set { documentedByCocDoc = value; }
+        }
+        public int Mileage
+        {
+            get { return mileage; }
+            set { mileage = value; }
+        }
+        public string OriginalPaintThickness
+        {
+            get { return originalPaintThickness; }
+            set { originalPaintThickness = value; }
+        }
+        public string OriginalPaintThicknessMeasured
+        {
+            get { return originalPaintThicknessMeasured; }
+            set { originalPaintThicknessMeasured = value; }
+        }
+        public string PaintConclusion
+        {
+            get { return paintConclusion; }
+            set { paintConclusion = value; }
+        }
+        public Boolean EngineNumberVerified
+        {
+            get { return engineNumberVerified; }
+            set { engineNumberVerified = value; }
+        }
+        public Boolean OriginalPaint
+        {
+            get { return originalPaint; }
+            set { originalPaint = value; }
+        }
+        public Boolean SerialCorrect
+        {
+            get { return serialCorrect; }
+            set { serialCorrect = value; }
+        }
+        public string SerialCondition
+        {
+            get { return serialCondition; }
+            set { serialCondition = value; }
+        }
+        public string SerialDamaged
+        {
+            get { return serialDamaged; }
+            set { serialDamaged = value; }
+        }
     }
 }
--- CSharpDotNetProject/Window2ListSearch.xaml.cs
+++ CSharpDotNetProject/Window2ListSearch.xaml.cs
@@ -19,9 +19,27 @@ namespace CSharpDotNetProject
     /// </summary>
     public partial class Window2ListSearch : Window
     {
+        List<CertificateElement> elements = new List<CertificateElement>();
+
         public Window2ListSearch()
         {
             InitializeComponent();
+            elements.Add(new CertificateElement() { serialNumber = "2520020", workShop = "Din mors auto", date = DateTime.Now });
+            elements.Add(new CertificateElement() { serialNumber = "2552520", workShop = "Dekra", date = DateTime.Now });
+            elements.Add(new CertificateElement() { serialNumber = "2534599", workShop = "Dekra", date = DateTime.Now });
+            certificateDGrid.ItemsSource = elements;
+        }
+        private void searchBox_KeyUp(object sender, System.Windows.Input.KeyboardEventArgs e)
+        {
+            List<CertificateElement> filtered = new List<CertificateElement>(elements.Where(certificate => certificate.serialNumber.StartsWith(searchBox.Text)));
+            certificateDGrid.ItemsSource = filtered;
         }
     }
+    public class CertificateElement
+    {
+        public string serialNumber{ get; set; }
+        public string workShop{ get; set; } 
+        public DateTime date { get; set; }
+    }
+    // create database connection to get current list. 
 }
