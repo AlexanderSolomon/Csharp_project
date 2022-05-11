9da11cca1df4cc244e7f0461bee8557b2aa60288
Fiedler12 <o.lyngholm@gmail.com>
2022-05-11 10:39:02 +0200

    Classes added

---
+++ CSharpDotNetProject/Certificate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDotNetProject
+{
+    internal class Certificate
+    {
+        private int id;
+        private 
+
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Certificate
+{
+	private int _id;
+	private string registration;
+	private string serialReg;
+	private string company;
+	private string inspector;
+
+
+}
