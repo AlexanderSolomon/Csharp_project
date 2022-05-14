ca990427239872e96dbcc27e4e06fd8be2306323
solom <s201172@student.dtu.dk>
2022-05-14 16:36:54 +0200

    HEJ

--- CSharpDotNetProject/MainWindow.xaml.cs
+++ CSharpDotNetProject/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace CSharpDotNetProject
       
         private void Openwindow1(object sender, RoutedEventArgs e)
         {
-            Window1 sW = new Window1();
+            WindowPictures sW = new WindowPictures();
             sW.Show();
             this.Close(); 
 
